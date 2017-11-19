dec6784c4c0714af4655cda0c00be5d097ca2330
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-20 02:29:14 +0800

    virgo: Remove libdivxdrmdecrypt package
    Change-Id: I3e8bb840b4650f749d6cec2ad35d72f9c7af28a9

--- virgo.mk
+++ virgo.mk
@@ -164,7 +164,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     libc2dcolorconvert \
-    libdivxdrmdecrypt \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
