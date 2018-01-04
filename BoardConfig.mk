55bf2d12cd4283c3fd55b851a84e3b669a3b4097
GuaiYiHu <guaiyihu@foxmail.com>
2018-01-04 15:35:39 +0800

    virgo: Mokee-ify
    Change-Id: I9dd2a5fdf102088ac015862c5c816b5c10c3359c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,8 +150,9 @@ TARGET_RECOVERY_FSTAB              := $(LOCAL_PATH)/rootdir/root/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT       := "RGBX_8888"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-# CM Hardware
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+# MK Hardware
+BOARD_USES_MOKEE_HARDWARE = true
+BOARD_HARDWARE_CLASS := device/xiaomi/virgo/mkhw
 
 # No old RPC for prop
 TARGET_NO_RPC := true
