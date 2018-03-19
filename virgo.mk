8394f2d04160bcf9266d17cce5e4d21446df344a
GuaiYiHu <guaiyihu@foxmail.com>
2018-03-19 10:45:21 +0800

    virgo: Switch to Mokee's custom vibrator HAL
    Change-Id: I46b11025c8de829cf137ae4aed31959131f5c6da

--- virgo.mk
+++ virgo.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.mokee
 
 #USB HAL
 PRODUCT_PACKAGES += \
