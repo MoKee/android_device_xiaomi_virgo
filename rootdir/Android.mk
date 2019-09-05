a4b75a26dd9dc5a6fda51108a4b73994b2721580
Han Wang <416810799@qq.com>
2019-09-05 07:07:51 +0200

    virgo: Remove LOCAL_MODULE_TAGS := eng usage
    Change-Id: I83aa494618d9c4809d3219aa3785e56b48ef8dfb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
